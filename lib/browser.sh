006a0b3666997229fad78982dd82c37ca5b37ca8
Ronmi Ren <ronmi.ren@gmail.com>
2019-03-29 23:36:53 +0800

    use local var to reduce side-effects

--- lib/browser.sh
+++ lib/browser.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/zsh -f
 
-_fx=${FIREFOX_PATH:-/usr/bin/firefox}
-_ch=${CHROME_PATH:-/usr/bin/google-chrome}
-
 function firefox {
+    _fx=${FIREFOX_PATH:-/usr/bin/firefox}
     p="${_RMI_WORK_HERE}/.rmi_work/firefox"
     mkdir -p "$p"
-    $_fx --profile "$p" "$@"
+    "$_fx" --profile "$p" "$@"
 }
 
 function google-chrome {
+    _ch=${CHROME_PATH:-/usr/bin/google-chrome}
     p="${_RMI_WORK_HERE}/.rmi_work/firefox"
     mkdir -p "$p"
-    $_ch --user-data-dir "$p" "$@"
+    "$_ch" --user-data-dir "$p" "$@"
 }
 
 function chrome {
