368e5a107ce6b0d8d20fef918c04c55c6132c3e1
Ronmi Ren <ronmi.ren@gmail.com>
2019-03-29 23:32:33 +0800

    prevent name collision

--- lib/browser.sh
+++ lib/browser.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/zsh -f
 
-fx=${FIREFOX_PATH:-/usr/bin/firefox}
-ch=${CHROME_PATH:-/usr/bin/google-chrome}
+_fx=${FIREFOX_PATH:-/usr/bin/firefox}
+_ch=${CHROME_PATH:-/usr/bin/google-chrome}
 
 function firefox {
     p="${_RMI_WORK_HERE}/.rmi_work/firefox"
     mkdir -p "$p"
-    $cmd --profile "$p" "$@"
+    $_fx --profile "$p" "$@"
 }
 
 function google-chrome {
     p="${_RMI_WORK_HERE}/.rmi_work/firefox"
     mkdir -p "$p"
-    $cmd --user-data-dir "$p" "$@"
+    $_ch --user-data-dir "$p" "$@"
 }
 
 function chrome {
