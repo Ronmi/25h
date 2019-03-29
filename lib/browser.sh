4bfc61873ccd61191a56f8de1abf1463ed1b5f96
Ronmi Ren <ronmi.ren@gmail.com>
2019-03-29 23:47:37 +0800

    separate chrome dir and add reset helper

--- lib/browser.sh
+++ lib/browser.sh
@@ -7,13 +7,21 @@ function firefox {
     "$_fx" --profile "$p" "$@"
 }
 
+function reset_firefox {
+    rm -fr "${_RMI_WORK_DIR}/firefox" > /dev/null 2>&1
+}
+
 function google-chrome {
     _ch=${CHROME_PATH:-/usr/bin/google-chrome}
-    p="${_RMI_WORK_DIR}/firefox"
+    p="${_RMI_WORK_DIR}/chrome"
     mkdir -p "$p"
     "$_ch" --user-data-dir "$p" "$@"
 }
 
 function chrome {
     google-chrome "$@"
+}
+
+function reset_chrome {
+    rm -fr "${_RMI_WORK_DIR}/chrome" > /dev/null 2>&1
 }
