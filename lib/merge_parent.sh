b2c4da888e42317509607c1b1caf91ee4a6fbae0
Ronmi Ren <ronmi.ren@gmail.com>
2019-09-22 16:06:23 +0800

    fix bugs when recursive merge parent config

--- lib/merge_parent.sh
+++ lib/merge_parent.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/zsh -f
 
+if [[ $__tmp_wh == "" ]]
+then
+    __tmp_wh="$_RMI_WORK_HERE"
+fi
 __tmp_parent="$(dirname "$_RMI_WORK_HERE")"
 
 if [[ -f "${__tmp_parent}/.rmi-work/conf.zsh" ]]
 then
+    # as if we're work there
+    _RMI_WORK_HERE="$__tmp_parent"
+    _RMI_WORK_DIR="$__tmp_parent/.rmi-work"
     source "${__tmp_parent}/.rmi-work/conf.zsh"
 fi
 
 unset __tmp_parent
+_RMI_WORK_HERE="$__tmp_wh"
+_RMI_WORK_DIR="$__tmp_wh/.rmi-work"
