0c311d840d0b91dadf86e4e34b16f58d040efba0
Ronmi Ren <ronmi.ren@gmail.com>
2025-06-26 20:25:05 +0800

    add helper to run android build tool

--- lib/android.sh
+++ lib/android.sh
@@ -4,3 +4,11 @@
 export ANDROID_HOME="${HOME}/Android/Sdk"
 export ANDROID_SDK_ROOT="${ANDROID_HOME}"
 export PATH="${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:${PATH}"
+
+function android_build_tool() {
+    sdkver="$(ls "${ANDROID_HOME}/build-tools"|grep -E '^[0-9]+\.[0-9]+\.[0-9]+$'|sort -r|head -n 1)"
+    cmd="$1"
+    shift
+
+    "${ANDROID_HOME}/build-tools/${sdkver}/${cmd}" "$@"
+}
