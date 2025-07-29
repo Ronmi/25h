da4053ea8802aad923efc3c48b30aad2d3c1855a
Ronmi Ren <ronmi.ren@gmail.com>
2025-07-29 15:45:18 +0800

    add new helper

---
+++ lib/dotenv.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/zsh -f
+# source .env automatically or on-demand
+
+# This special envvar is used to enable autoloading
+if [[ "$_RMI_DOTENV_AUTOLOAD" == "1" && -f "${_RMI_WORK_HERE}/.env" ]]
+then
+    source "${_RMI_WORK_HERE}/.env"
+fi
+
+# create a subshell and load .env.
+function useenv() {
+    local f="${1:-${_RMI_WORK_HERE}/.env}"
+    if [[ ! -f "$f" ]]
+    then
+        echo "no env file found in ${f}"
+        echo
+        echo "Usage: useenv [env_file]"
+        echo
+        echo "It will use ${_RMI_WORK_HERE}/.env if no env_file specified"
+        return 1
+    fi
+    (
+        source "$f"
+        "$SHELL"
+    )
+}
