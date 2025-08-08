a2c3feead0af451b47e9d358bb1576257e32977f
Ronmi Ren <ronmi.ren@gmail.com>
2025-08-08 15:29:56 +0800

    refine dotenv helper

--- lib/dotenv.sh
+++ lib/dotenv.sh
@@ -1,26 +1,40 @@
 #!/usr/bin/zsh -f
-# source .env automatically or on-demand
+# source .env automatically or on-demand, set _RMI_DOTENV_AUTOLOAD to 1 (or a file) to enable autoloading, or run `useenv` to load a specific .env file in a subshell
+
+_env_file="${_RMI_WORK_HERE}/.env"
+local _do=0
 
 # This special envvar is used to enable autoloading
-if [[ "$_RMI_DOTENV_AUTOLOAD" == "1" && -f "${_RMI_WORK_HERE}/.env" ]]
+if [[ "$_RMI_DOTENV_AUTOLOAD" == "1" ]]
 then
-    source "${_RMI_WORK_HERE}/.env"
-fi
-if [[ "$_RMI_DOTENV_AUTOLOAD" != "" && -f "$_RMI_DOTENV_AUTOLOAD" ]]
+    _env_file="${_RMI_WORK_HERE}/.env"
+    _do=1
+elif [[ "$_RMI_DOTENV_AUTOLOAD" != "" ]]
 then
-    source "$_RMI_DOTENV_AUTOLOAD"
+    _env_file="${_RMI_DOTENV_AUTOLOAD}"
+    if [[ "$_env_file" != /* ]]
+    then
+        _env_file="${_RMI_WORK_HERE}/${_env_file}"
+    fi
+    _do=1
 fi
+
+if [[ _do -eq 1 && -f "$_env_file" ]]
+then
+    source "$_env_file"
+fi
+unset _do
 
 # create a subshell and load .env.
 function useenv() {
-    local f="${1:-${_RMI_WORK_HERE}/.env}"
+    local f="${1:-${_env_file}}"
     if [[ ! -f "$f" ]]
     then
         echo "no env file found in ${f}"
         echo
         echo "Usage: useenv [env_file]"
         echo
-        echo "It will use ${_RMI_WORK_HERE}/.env if no env_file specified"
+        echo "It will use ${_env_file} if no env_file specified"
         return 1
     fi
     (
