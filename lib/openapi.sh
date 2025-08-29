c1adca155d7b5671e5eddd20127e7d1f1affc82f
Ronmi Ren <ronmi.ren@gmail.com>
2025-08-29 23:47:59 +0800

    fix swagger ui helper: fix envvar and support relative path

--- lib/openapi.sh
+++ lib/openapi.sh
@@ -14,9 +14,16 @@ function openapi_local_srv() {
         echo "Please set OPENAPI_SPEC_FILE to the path of your openapi spec file (openapi.json/swagger.json)"
         return 1
     fi
-    local full_path="$(realpath $OPENAPI_SPEC_FILE)"
-    local fn="$(basename "$full_path")"
-    docker run -d --rm -p 11111:8080 -e "SWAGGER_JSON=/$fn" -v "$full_path:/$fn:ro" docker.swagger.io/swaggerapi/swagger-ui
+    (
+        backroot
+        local full_path="$(realpath $OPENAPI_SPEC_FILE)"
+        local fn="$(basename "$full_path")"
+        local name="$(basename "$_RMI_WORK_HERE" | sed 's/[ ]/-/')-swagger-ui"
+        docker run -d --rm --name "$name" -p 11111:8080 -e "SWAGGER_JSON=/$fn" -v "${full_path}:/${fn}:ro" docker.swagger.io/swaggerapi/swagger-ui || return $?
+
+        echo "you can visit http://localhost:11111 to view the docs"
+        echo "to stop the server, run: docker stop $name"
+    )
 }
 
 # generate html documentation using redocly
